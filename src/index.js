abd7c659e9a0cf49176d89292f9dd600ebf99838
Nick Fahey <nfahey@live.com.au>
2025-11-29 12:55:48 +1100

    feat: Implement helmet, cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
 
+// Security
+const helmet = require('helmet');
+const cors = require('cors');
+
+app.use(helmet());
+app.use(
+	cors({
+		origin: ['http://127.0.0.1:5000'],
+		optionsSuccessStatus: 200,
+	})
+);
+
+// Root Route
 app.get('/', (req, res) => {
-    res.json({ status: 'Ok' });
+	res.json({ status: 'Ok' });
 });
 
-module.exports = { app }
+module.exports = { app };
