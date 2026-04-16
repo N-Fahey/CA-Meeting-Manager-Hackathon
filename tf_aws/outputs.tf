ba8a571d3b4fff268a501f464d8d3fba740f622f
Nick Fahey <nfahey@live.com.au>
2026-04-16 10:27:50 +1000

    Remove ECS configuration from Terraform

--- tf_aws/outputs.tf
+++ tf_aws/outputs.tf
@@ -9,15 +9,3 @@ output "ecr_repo_arn" {
 output "ecr_repo_url" {
   value = module.resources.ecr_repo_url
 }
-
-output "ecs_cluster_arn" {
-  value = module.resources.ecs_cluster_arn
-}
-
-output "sm_mongo_uri_secret_arn" {
-  value = module.resources.sm_mongo_uri_secret_arn
-}
-
-output "sm_jwt_secret_arn" {
-  value = module.resources.sm_jwt_secret_arn
-}
--- tf_aws/resources/outputs.tf
+++ tf_aws/resources/outputs.tf
@@ -9,15 +9,3 @@ output "ecr_repo_arn" {
 output "ecr_repo_url" {
   value = aws_ecr_repository.app_ecr_repo.repository_url
 }
-
-output "ecs_cluster_arn" {
-  value = aws_ecs_cluster.ecs_cluster.arn
-}
-
-output "sm_mongo_uri_secret_arn" {
-  value = aws_secretsmanager_secret.sm_mongo_uri.arn
-}
-
-output "sm_jwt_secret_arn" {
-  value = aws_secretsmanager_secret.sm_jwt_secret.arn
-}
